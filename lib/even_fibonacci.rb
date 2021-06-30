db778177cd67dedd1e83541768e3c70374f91f35
soncrayon <arnold.curtis@gmail.com>
2021-06-30 19:43:19 -0400

    Done.

--- lib/even_fibonacci.rb
+++ lib/even_fibonacci.rb
@@ -1,14 +1,17 @@
 # Implement your procedural solution here!
 def even_fibonacci_sum(limit)
-    fibonacci_arr = []
-    for x in 0..(limit - 1) do 
-        if fibonacci_arr.length == 0 then 
-            fibonacci_arr << 0
-        elsif fibonacci_arr.length == 1 then
+    fibonacci_arr = [0]
+    x=0 
+    while fibonacci_arr[-1] < (limit - 1) 
+       if fibonacci_arr.length == 1 
             fibonacci_arr << 1
-        else
-            fibonacci_arr << (fibonacci_arr[i -1] + fibonacci_arr[i -2])
+        else  
+            if (fibonacci_arr[x]+fibonacci_arr[x - 1]) < limit
+                fibonacci_arr << (fibonacci_arr[x]+fibonacci_arr[x - 1])
+            else
+                return fibonacci_arr.select{|num| num % 2 == 0}.sum
+            end
         end
+        x = x+1
     end
-    return fibonacci_arr.select{|num| num % 2 == 0}.sum
 end
--- lib/oo_even_fibonacci.rb
+++ lib/oo_even_fibonacci.rb
@@ -1,1 +1,27 @@
 # Implement your object-oriented solution here!
+class EvenFibonacci
+
+    attr_accessor :limit
+
+    def initialize(limit)
+        @limit = limit
+    end
+
+    def sum
+        fibonacci_arr = [0]
+        x=0 
+        while fibonacci_arr[-1] < (limit - 1) 
+           if fibonacci_arr.length == 1 
+                fibonacci_arr << 1
+            else  
+                if (fibonacci_arr[x]+fibonacci_arr[x - 1]) < limit
+                    fibonacci_arr << (fibonacci_arr[x]+fibonacci_arr[x - 1])
+                else
+                    return fibonacci_arr.select{|num| num % 2 == 0}.sum
+                end
+            end
+            x = x+1
+        end
+    end
+
+end
