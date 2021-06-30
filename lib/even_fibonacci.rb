9aa4e274b432e24689c0efafd1c2e6f2561a4a1e
soncrayon <arnold.curtis@gmail.com>
2021-06-29 21:54:46 -0400

    Done.

--- lib/even_fibonacci.rb
+++ lib/even_fibonacci.rb
@@ -1,1 +1,14 @@
 # Implement your procedural solution here!
+def even_fibonacci_sum(limit)
+    fibonacci_arr = []
+    for x in 0..(limit - 1) do 
+        if fibonacci_arr.length == 0 then 
+            fibonacci_arr << 0
+        elsif fibonacci_arr.length == 1 then
+            fibonacci_arr << 1
+        else
+            fibonacci_arr << (fibonacci_arr[i -1] + fibonacci_arr[i -2])
+        end
+    end
+    return fibonacci_arr.select{|num| num % 2 == 0}.sum
+end
